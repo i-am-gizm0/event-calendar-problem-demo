9e92f6c07e3b0a75e785e262484c30dbe08b4b8d
Sam <centersam19@gmail.com>
2022-12-29 14:55:15 -0500

    Bare minimum demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Calendar from '@event-calendar/core';
+    import TimeGrid from '@event-calendar/time-grid';
+
+    let plugins = [TimeGrid];
+    let options = {
+        view: 'timeGridWeek',
+        events: []
+    };
+</script>
+
+<Calendar {plugins} {options} />
